aa9ebd90728f965f6a11385e3602ba6596a73b41
Waldo Hidalgo <whidalgohp@gmail.com>
2024-01-26 12:10:30 -0300

    finalizo footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,3 +50,15 @@ const tooltipTriggerList = [].slice.call(
 const tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl);
 });
+
+/*-----------------------------------------------------------------*/
+/*
+Habilitacion de popover (creacion de objetos popover)
+*/
+
+const popoverTriggerList = [].slice.call(
+  document.querySelectorAll('[data-bs-toggle="popover"]')
+);
+const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl);
+});
